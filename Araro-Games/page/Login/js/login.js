869011ff510d29805004b5742dae7ecb99647448
Ikari <164592538+RVPutian@users.noreply.github.com>
2026-05-01 20:01:58 +0800

    Delete Araro-Games directory

--- Araro-Games/page/Login/js/login.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const firstInput = document.querySelector('.auth-field .form-input');
-    if (firstInput) {
-        firstInput.focus();
-    }
-});
