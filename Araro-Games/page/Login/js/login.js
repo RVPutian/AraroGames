7bef1d1b51af6aef26fc8bf95006ab75d455e847
Ikari <164592538+RVPutian@users.noreply.github.com>
2026-05-01 20:01:18 +0800

    Add files via upload

---
+++ Araro-Games/page/Login/js/login.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const firstInput = document.querySelector('.auth-field .form-input');
+    if (firstInput) {
+        firstInput.focus();
+    }
+});
